b7cfebb46faa4ff042592ef4383e7720f8d3a918
Kevin Holder <kholder777@yahoo.com>
2020-07-08 13:02:41 -0500

    modified leveraged data to better suite our targeted values.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,11 +1,12 @@
 // ===============================================================================
 // LOAD DATA
 // We are linking our routes to a series of "data" sources.
+var fs = require("fs");
 // These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
 
-var tableData = require("../data/tableData");
-var waitListData = require("../data/waitinglistData");
+var notesData = require("../data/notesData");
+// var waitListData = require("../data/waitinglistData");
 
 // ===============================================================================
 // ROUTING
@@ -18,8 +19,8 @@ module.exports = function (app) {
 	// (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
 	// ---------------------------------------------------------------------------
 
-	app.get("/api/tables", function (req, res) {
-		res.json(tableData);
+	app.get("/api/notes", function (req, res) {
+		res.json(notesData);
 	});
 
 	app.get("/api/waitlist", function (req, res) {
@@ -34,12 +35,12 @@ module.exports = function (app) {
 	// Then the server saves the data to the tableData array)
 	// ---------------------------------------------------------------------------
 
-	app.post("/api/tables", function (req, res) {
+	app.post("/api/notes", function (req, res) {
 		// Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
 		// It will do this by sending out the value "true" have a table
 		// req.body is available since we're using the body parsing middleware
-		if (tableData.length < 5) {
-			tableData.push(req.body);
+		if (noteData.length < 5) {
+			noteData.push(req.body);
 			res.json(true);
 		} else {
 			waitListData.push(req.body);
