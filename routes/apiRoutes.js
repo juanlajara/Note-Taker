cbf82edbafe44bafb3fab1ae5e2c6314cc40ffe1
juanlajara <juanlajara001@gmail.com>
2020-07-09 22:46:50 -0500

    tutoring sessions - not much here

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,44 +5,22 @@ var fs = require("fs");
 // These data sources hold information on notes.
 // ===============================================================================
 var notesData = require("../db/db.json");
-// var waitListData = require("../data/waitinglistData");
 
 // ===============================================================================
 // ROUTING
 // ===============================================================================
 
 module.exports = function (app) {
-	// API GET Requests
-	// Below code handles when users "visit" a page.
-	// In each of the below cases when a user visits a link
-	// (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-	// ---------------------------------------------------------------------------
-
 	app.get("/api/notes", function (req, res) {
-		res.json(notesData);
+		console.log(req);
 	});
-
-	// API POST Requests
-	// Below code handles when a user submits a form and thus submits data to the server.
-	// In each of the below cases, when a user submits form data (a JSON object)
-	// ...the JSON is pushed to the appropriate JavaScript array
-	// ---------------------------------------------------------------------------
 
 	app.post("/api/notes", function (req, res) {
-		// Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-		// It will do this by sending out the value "true" have a table
-		// req.body is available since we're using the body parsing middleware
-		noteData.push(req.body);
-
+		notesData.push(req.body);
 	});
 
-	// ---------------------------------------------------------------------------
-	// I added this below code so you could clear out the table while working with the functionality.
-	// Don"t worry about it!
-
 	app.post("/api/notes/:id", function (req, res) {
-		// Empty out the arrays of data
-		// noteData.length = 0;
-		// res.json({ ok: true });
+		var id = req.body.id;
+		notesData = notesData.filter((e) => !e.id == id);
 	});
 };
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -22,7 +22,4 @@ module.exports = function (app) {
 	app.get("*", function (req, res) {
 		res.sendFile(path.join(__dirname, "../public/index.html"));
 	});
-
-	
-
 };
